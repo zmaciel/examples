f4273b0547108450835662c597af9821e3e0e164
zmaciel <46382529+zmaciel@users.noreply.github.com>
2021-03-27 23:15:50 +0100

    Update handler.js

--- aws-node-simple-http-endpoint/handler.js
+++ aws-node-simple-http-endpoint/handler.js
@@ -7,6 +7,6 @@ module.exports.endpoint = (event, context, callback) => {
       message: `Hello, the current time is ${new Date().toTimeString()}.`,
     }),
   };
-
+ 
   callback(null, response);
 };
